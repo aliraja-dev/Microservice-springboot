9b34fc2310f51d740ac4f6cce88146282235902e
Ali Raja <aliraja.en@gmail.com>
2022-05-30 12:26:27 -0400

    added: Eureka Service Registry Server Project- Initial Commit

---
+++ service-registry/src/main/java/com/alirajadev/registry/EurekaServiceRegistryServerApplication.java
@@ -0,0 +1,13 @@
+package com.alirajadev.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceRegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceRegistryServerApplication.class, args);
+	}
+
+}
