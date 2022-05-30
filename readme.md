581a81904d7d135af4407e6f8886c755d0633b7a
Ali Raja <aliraja.en@gmail.com>
2022-05-30 12:34:44 -0400

    setup: eureka server port & annotation

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Micro-services using Spring Boot, Docker & Kubernetes
+
+Creating two Micro services for Employee and Departments on Port 9002 & 9001
+
+Setting up a Service Registry using Eureka Server
+Listening at Port 8761
--- service-registry/src/main/java/com/alirajadev/registry/EurekaServiceRegistryServerApplication.java
+++ service-registry/src/main/java/com/alirajadev/registry/EurekaServiceRegistryServerApplication.java
@@ -2,8 +2,10 @@ package com.alirajadev.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceRegistryServerApplication {
 
 	public static void main(String[] args) {
