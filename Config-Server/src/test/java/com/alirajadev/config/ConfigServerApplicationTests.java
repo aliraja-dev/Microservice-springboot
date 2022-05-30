109d2bb3f6821954012daf680a1856d70d208770
Ali Raja <aliraja.en@gmail.com>
2022-05-30 16:57:43 -0400

    added: Config server for registering with Eureka thru Github

---
+++ Config-Server/src/test/java/com/alirajadev/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alirajadev.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
